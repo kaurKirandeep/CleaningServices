2d2d11a6b8668a9c9a92fdcdea31f8032d3505f2
kaurKirandeep <70545375+kaurKirandeep@users.noreply.github.com>
2021-01-12 14:23:32 -0500

    files uploaded

---
+++ cleaningServices/js/custom.js
@@ -0,0 +1,30 @@
+// window.onload = function() {
+//     if (window.jQuery) {  
+//         // jQuery is loaded  
+//         alert("Yeah!");
+//     } else {
+//         // jQuery is not loaded
+//         alert("Doesn't Work");
+//     }
+// }
+
+
+// document.getElementsByClassName("topnav").onclick = function() {myFunction()};
+
+// function myFunction() {
+//   document.getElementsByClassName("mobile_section").style.display="block";
+// }
+
+// jQuery(document).ready(function() {
+//     jQuery('.menu-trigger').click(function() {
+//       jQuery('.mydiv').slideToggle(500);
+//     });//end slide toggle
+
+// jQuery( ".topnav" ).click(function() {
+//   jQuery( ".mobile-top-navigation" ).toggle()
+//     });
+
+
+    jQuery( ".topnav" ).click(function() {
+      jQuery( ".mobile-top-navigation" ).toggle();
+    });
